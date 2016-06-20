d0ba3ea53780cbe061e9600d6168de2c641fe160
bmanjuu <bettyofung@gmail.com>
2016-06-20 09:31:30 -0400

    lab complete

--- FISGorgeOfEternalPerilViewController.m
+++ FISGorgeOfEternalPerilViewController.m
@@ -2,6 +2,22 @@
 
 #import "FISGorgeOfEternalPerilViewController.h"
 
+@interface FISGorgeOfEternalPerilViewController()
+- (IBAction)acceptFateTapped:(id)sender;
+@end
+
 @implementation FISGorgeOfEternalPerilViewController
+
+//-(void)viewDidLoad {
+//    [super viewDidLoad];
+//}
+//
+//-(void)didReceiveMemoryWarning{
+//    [super didReceiveMemoryWarning];
+//}
+- (IBAction)acceptFateTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
